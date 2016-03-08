93490c53135247a8db67150d0f279188a6d9da2b
Zhao Yuanjie <qq157755587@gmail.com>
2016-03-08 12:30:44 +0800

    可以从首页添加白名单了

--- app/src/main/kotlin/com/zhaoyuanjie/adphoneblocker/ui/MainActivity.kt
+++ app/src/main/kotlin/com/zhaoyuanjie/adphoneblocker/ui/MainActivity.kt
@@ -8,7 +8,9 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.text.InputType
 import android.view.View
+import android.widget.EditText
 import com.zhaoyuanjie.adphoneblocker.R
 import com.zhaoyuanjie.adphoneblocker.util.AppPreferences
 import kotlinx.android.synthetic.main.activity_main.*
@@ -62,11 +64,32 @@ class MainActivity: AppCompatActivity(), WhiteListAdapter.WhiteListListener {
     private fun showWhiteList() {
         val list = pref.getWhiteList()
         if (list.size > 0) {
+            supportActionBar?.setSubtitle(R.string.white_list)
+            // 显示白名单和按钮
             mAdapter = WhiteListAdapter(list, this)
             recycler_view.layoutManager = LinearLayoutManager(this)
             recycler_view.adapter = mAdapter
             recycler_view.visibility = View.VISIBLE
-            supportActionBar?.setSubtitle(R.string.white_list)
+            action_button.visibility = View.VISIBLE
+            // 点击事件
+            action_button.setOnClickListener { addNumberToWhiteList() }
         }
+    }
+
+    private fun addNumberToWhiteList() {
+        val editText = EditText(this)
+        editText.inputType = InputType.TYPE_CLASS_PHONE
+
+        AlertDialog.Builder(this)
+                .setTitle(R.string.add_to_white_list)
+                .setView(editText)
+                .setPositiveButton(android.R.string.ok, { dialog, which ->
+                    val number = editText.text.trim().toString()
+                    pref.addNumberToWhiteList(number)
+                    mAdapter.list.add(number)
+                    mAdapter.notifyItemInserted(mAdapter.itemCount - 1)
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
     }
 }
